e2ba7ace522256c47fecc0392ca1347553913ee9
Aditya Pathak <46691394+adityasunny1189@users.noreply.github.com>
2021-05-04 00:31:16 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
 import requests
 import json
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def api_call():
