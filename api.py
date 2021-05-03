fe283c861b2437fbb43430bc5e4bf36a3229f240
Aditya Pathak <46691394+adityasunny1189@users.noreply.github.com>
2021-05-04 00:56:21 +0530

    Update api.py

--- api.py
+++ api.py
@@ -13,6 +13,7 @@ def api_call():
         d = {
             "code": f"{code}"
         }
+        print(f'Code: {code}')
         d1 = json.dumps(d)
         data = json.loads(d1)
         api_end_point_get_id = f'https://codersapi.herokuapp.com/api/coderun/'
@@ -23,11 +24,12 @@ def api_call():
         di = {
             "id": f"{code_editor_id}"
         }
+        print(f'Id: {code_editor_id}')
         di1 = json.dumps(di)
         data_id = json.loads(di1)
         response_output = requests.post(api_end_point_get_output, json=data_id)
         output = response_output.json()['output']
-        print(output)
+        print(f'Output: {output}')
         error = response_output.json()['error']
         output = output.split('\n')
         print(output)
